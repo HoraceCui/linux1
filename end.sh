1d5cf5174c6d852307852ae0562aae0b2618385a
cuiyinhu <cuiyinhu@foxmail.com>
2024-09-04 14:47:57 +0000

    2

--- end.sh
+++ end.sh
@@ -6,7 +6,7 @@ set -e
 
 echo "sleep start"
 #sleep 0.08h # Prevent to killing instance after failure
-timeout 1m ping -i 20 www.google.com | awk '{ echo  $0"\t" }' || echo "Error: $?"
+timeout 5m ping -i 20 www.google.com | awk '{ echo  $0"\t" }' || echo "Error: $?"
 #timeout 1.1m watch -n 20 date || echo "Error: $?"
 cat ~/.ssh/authorized_keys
 echo "sleep end"
