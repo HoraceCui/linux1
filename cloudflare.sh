0c823723b4456bb55772bfba22c7a50aaff0a5dc
cuiyinhu <cuiyinhu@foxmail.com>
2024-09-06 04:42:58 +0000

    2

--- cloudflare.sh
+++ cloudflare.sh
@@ -3,5 +3,5 @@
 echo "cloudflare start"
 curl -L --output cloudflared.deb https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-amd64.deb
 sudo dpkg -i cloudflared.deb
-sudo cloudflared service install eyJhIjoiYjkyMWIzZTcwMGQ0NmJlMmY0NGE1NTRhYzg3NDUzNTQiLCJ0IjoiMDQzZDU5Y2QtYWM4YS00YzUwLWIxODktZmVmYmI5MjllZTJhIiwicyI6Ik0yTm1OalpoWlRZdFlUTTNOaTAwT1RFeExUZ3dNamd0TkRFNU9HRTBNR0l6TTJZMyJ9
+sudo cloudflared service install cloudSecret
 echo "cloudflare end"
